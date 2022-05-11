5191390b61efdaee2a987e4019471224d8b1ee7c
Aidan Liddiard <95311213+aidanliddiard@users.noreply.github.com>
2022-05-10 17:03:39 -0700

    initial entries test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,7 +6,7 @@ import App from './App';
 describe('App', () => {
   it('renders Rick and Morty characters', async () => {
     render(
-      <MemoryRouter initialEntries={['/']}>
+      <MemoryRouter>
         <App />
       </MemoryRouter>
     );
@@ -19,7 +19,15 @@ describe('App', () => {
     const link = await screen.findByText('Morty Smith');
     userEvent.click(link);
     await screen.findByText(/status: alive/i);
-
+  });
+  it('displays the details page', async () => {
+    render(
+      <MemoryRouter initialEntries={['/', '/character/5']} initialIndex={1}>
+        <App />
+      </MemoryRouter>
+    );
+    screen.getByText('Back to Home');
+    await screen.findByText('Jerry Smith');
     screen.debug();
   });
 });
